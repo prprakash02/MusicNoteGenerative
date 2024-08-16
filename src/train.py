b3f42305a03eae7afb5697bcbc21073a6136ec97
joel <joelwoodfield@gmail.com>
2024-08-16 23:04:17 +1000

    Fix model saving directory issue and add tensorboard loss logging

--- src/train.py
+++ src/train.py
@@ -1,18 +1,24 @@
 import os
+
 from datetime import datetime
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
 from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
 
 from src.loading import BachDataset
 from src.model import Autoencoder
 
 
+
 def train_model():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    writer = SummaryWriter(os.path.join('models', CURRENT_TIME))
+
     model = Autoencoder().to(device)
     dataset = BachDataset("bach-dataset")
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
@@ -20,7 +26,7 @@ def train_model():
     criterion = nn.MSELoss()
     n_epochs = 100
 
-    train_loop(model, criterion, optimizer, dataloader, n_epochs=n_epochs)
+    train_loop(model, criterion, optimizer, dataloader, n_epochs=n_epochs, writer=writer)
 
 
 def train_loop(
@@ -28,12 +34,18 @@ def train_loop(
     criterion: nn.Module,
     optimizer: optim.Optimizer,
     dataloader: torch.utils.data.DataLoader,
-    n_epochs: int = 100
+    n_epochs: int = 100,
+    writer: SummaryWriter = None,
 ) -> None:
     for epoch in tqdm(range(n_epochs)):
+        losses = []
         for sample in dataloader:
-            train_step(model, criterion, optimizer, sample)
+            train_step(model, criterion, optimizer, sample, losses)
+
         save_model(model, epoch)
+        avg_loss = np.mean(losses)
+        if writer is not None:
+            writer.add_scalar('loss', avg_loss, epoch)
 
 
 def train_step(
@@ -41,6 +53,7 @@ def train_step(
     criterion: nn.Module,
     optimizer: optim.Optimizer,
     sample: torch.Tensor,
+    losses: list[float],
 ) -> None:
     sample = sample.to(next(model.parameters()).device)
     reconstruction = model(sample)
@@ -50,8 +63,10 @@ def train_step(
     loss.backward()
     optimizer.step()
 
+    losses.append(loss.item())
 
-def save_model(model: nn.Module, epoch: int) -> None:
+
+def get_time():
     now = datetime.now()
     year = now.year
     month = now.month
@@ -59,11 +74,15 @@ def save_model(model: nn.Module, epoch: int) -> None:
     hour = now.hour
     minute = now.minute
     time = f"{year}-{month}-{day}-{hour}-{minute}"
+    return time
 
-    models_directory = os.path.join("models", time)
+
+def save_model(model: nn.Module, epoch: int) -> None:
+    models_directory = os.path.join("models", CURRENT_TIME)
     os.makedirs(models_directory, exist_ok=True)
     torch.save(model.state_dict(), os.path.join(models_directory, f"{epoch}.pt"))
 
 
 if __name__ == "__main__":
+    CURRENT_TIME = get_time()
     train_model()
