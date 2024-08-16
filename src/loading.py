29d9db83fabf6100f40a636023398b556979db6d
joel <joelwoodfield@gmail.com>
2024-08-16 22:32:15 +1000

    Add model training code

---
+++ src/loading.py
@@ -0,0 +1,44 @@
+import glob
+import os
+
+import numpy as np
+import librosa
+import torch
+
+N_MELS = 128
+
+
+def get_spectrogram(audio_file: str) -> np.ndarray:
+    amplitudes, sr = librosa.load(audio_file)
+    spectrogram = librosa.feature.melspectrogram(y=amplitudes, sr=sr, n_mels=N_MELS)
+    spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
+    return spectrogram
+
+
+class BachDataset(torch.utils.data.Dataset):
+    def __init__(self, data_root_path: str):
+        mp3_files = []
+        audio = os.path.join(data_root_path, "audio")
+        for artist in os.listdir(audio):
+            artist_path = os.path.join(audio, artist)
+            mp3_files.extend(glob.glob(artist_path + "/*.mp3"))
+
+        self.mp3_files = mp3_files
+
+    def __len__(self):
+        return len(self.mp3_files)
+
+    def __getitem__(self, idx):
+        audio_file = self.mp3_files[idx]
+        spectrogram = get_spectrogram(audio_file)
+        spectrogram = torch.Tensor(spectrogram)
+        return spectrogram
+
+
+def test():
+    dataset = BachDataset(data_root_path="../bach-dataset")
+    test = dataset[1]
+
+
+if __name__ == "__main__":
+    test()
---
+++ src/model.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.activation = nn.ReLU()
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.activation(x)
+        x = self.conv2(x)
+        x = self.activation(x)
+        return x
+
+    def extract_notes(self, x):
+        x = self.forward(x)
+        # something like this x = torch.argmax(x, dim=1)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.tconv1 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=1, padding=1)
+        self.activation = nn.ReLU()
+        self.tconv2 = nn.ConvTranspose2d(32, 1, kernel_size=3, stride=1, padding=1)
+        self.output = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.tconv1(x)
+        x = self.activation(x)
+        x = self.tconv2(x)
+        return x
+
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+    def extract_notes(self, x):
+        return self.encoder.extract_notes(x)
